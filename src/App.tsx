b4c015dd9c1f2a8ec1eaf5373d20e5a09ffa96a2
Marc0sGabriel <mgabriel4747@gmail.com>
2023-06-19 17:40:52 -0300

    feat: Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
-      <div>
-        <h1>Hello world</h1>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="success" />
-        <Button variant="danger" />
-        <Button />
-      </div>
     </ThemeProvider>
   );
 }
