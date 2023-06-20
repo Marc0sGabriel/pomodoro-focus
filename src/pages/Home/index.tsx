addbe786f6986b7e8bd86209fb0a7b3cb141d9b1
Marc0sGabriel <mgabriel4747@gmail.com>
2023-06-20 16:48:24 -0300

    :sparkles: feat: start countdown button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,31 @@
+import { Play } from '@phosphor-icons/react';
+
 export function Home() {
   return (
     <div>
       <form action="">
-        <label htmlFor="">Vou focar em</label>
-        <input id="task" />
+        <div>
+          <label htmlFor="">Vou focar em</label>
+          <input id="task" />
 
-        <label htmlFor="">durante</label>
-        <input type="number" id="minutesAmount" />
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
 
-        <span>minutos.</span>
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
       </form>
     </div>
   );
