76149d69d4e47762818eb4a083b61b489f279964
Marc0sGabriel <mgabriel4747@gmail.com>
2023-06-22 20:18:28 -0300

    :lipstick: styles: Main Inputs Home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -28,9 +28,34 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `;
 
-export const TaskInput = styled.input``;
+const BaseInput = styled.input`
+  background-color: transparent;
+  border: none;
+  height: 2.5rem;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
 
-export const MinutesAmountInput = styled.input``;
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['pink-700']};
+    transition: all 0.3s;
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
 
 export const CountDownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
@@ -45,6 +70,7 @@ export const CountDownContainer = styled.div`
     background-color: ${(props) => props.theme['gray-700']};
     padding: 2rem 1rem;
     border-radius: 8px;
+    border: 1px solid ${(props) => props.theme['gray-600']};
   }
 `;
 
