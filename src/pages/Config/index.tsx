ebd57fe22f456eff862879f2948944ff0a68d2ee
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-10 16:35:33 -0300

    :sparkles: feat: new page, settings page

---
+++ src/pages/Config/index.tsx
@@ -0,0 +1,9 @@
+import { ConfigPageContainer } from './styles';
+
+export function SettingsPage() {
+  return (
+    <ConfigPageContainer>
+      <h1>Configurações</h1>
+    </ConfigPageContainer>
+  );
+}
