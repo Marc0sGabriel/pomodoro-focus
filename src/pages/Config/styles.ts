530b0a67252edb00ed3905b77fe6ba8588a40d3b
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-10 16:35:53 -0300

    :lipstick: feat: styles settings page

---
+++ src/pages/Config/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const ConfigPageContainer = styled.div`
+  padding: 3.5rem;
+  h1 {
+    font-size: 1.75rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`;
