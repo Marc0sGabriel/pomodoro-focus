c7ffe97d99d03302290fa28ab572d0a7fd683207
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-13 09:16:27 -0300

    :feat: custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
 import { CyclesContextProvider } from './context/CyclesContext';
+import { draculaTheme } from './styles/themes/default';
+import { useState } from 'react';
 
 export function App() {
+  const [customTheme, setCustomTheme] = useState(draculaTheme);
+
+  const handleThemeChange = (theme: DefaultTheme) => {
+    setCustomTheme(theme);
+  };
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <CyclesContextProvider>
+    <ThemeProvider theme={customTheme}>
+      <CyclesContextProvider handleThemeChange={handleThemeChange}>
+        <BrowserRouter>
           <Router />
-        </CyclesContextProvider>
-      </BrowserRouter>
+        </BrowserRouter>
+      </CyclesContextProvider>
 
       <GlobalStyle />
     </ThemeProvider>
