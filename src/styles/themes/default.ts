afec3da82c9f9d7ac796fe178de561dcedfed0d3
Marc0sGabriel <mgabriel4747@gmail.com>
2023-08-13 09:23:09 -0300

    :themes: dracula theme as default

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,6 +1,29 @@
-export const defaultTheme = {
+export const draculaTheme = {
+  title: 'dracula',
   white: '#fff',
 
+  colors: {
+    primary: '#bd93f9',
+    secondary: '#ff79c6',
+    highlight: '#8be9fd',
+    background: '#282a36',
+    foreground: '#44475a',
+    selection: '#ffb86c',
+    green: '#50fa7b',
+    red: '#ff5555',
+    yellow: '#f1fa8c',
+
+    gray: {
+      'gray-100': '#E1E1E6',
+      'gray-200': '#C4C4CC',
+      'gray-300': '#8D8D99',
+      'gray-500': '#7C7C8A',
+      'gray-600': '#323238',
+      'gray-700': '#29292E',
+      'gray-800': 'rgba(32,32,36, 0.8)',
+      'gray-900': '#121214',
+    },
+  },
   'gray-100': '#E1E1E6',
   'gray-200': '#C4C4CC',
   'gray-300': '#8D8D99',
@@ -9,28 +32,7 @@ export const defaultTheme = {
   'gray-700': '#29292E',
   'gray-800': 'rgba(32,32,36, 0.8)',
   'gray-900': '#121214',
-
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
-
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
-
-  'pink-300': '#f4b8da',
-  'pink-400': '#eea1cd',
-  'pink-700': '#e68bbe',
-  'pink-800': '#F266AB',
-
-  'yellow-500': '#FBA94C',
-
   'dracula-red': '#ff5555',
-  'dracula-cyan': '#8be9fd',
-  'dracula-pink': '#ff79c6',
   'dracula-green': '#50fa7b',
-  'dracula-purple': '#bd93f9',
   'dracula-yellow': '#f1fa8c',
-  'dracula-orange': '#ffb86c',
-  'dracula-foreground': '#44475a',
-  'dracula-background': '#282a36',
 };
