139a2c06f96e909a9cd3656766491ef679d64097
Marc0sGabriel <mgabriel4747@gmail.com>
2023-09-28 19:19:53 -0300

    :lipstick: styles: updated default layout style

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const LayoutContainer = styled.div`
   max-width: 74rem;
   height: calc(100vh - 10rem);
-  margin: 5rem auto;
+  margin: 1rem auto;
   padding: 2.5rem;
   border-radius: 8px;
-  background-color: ${(props) => props.theme['gray-800']};
-  backdrop-filter: blur(8px);
+  background-color: rgba(12, 12, 12, 0.4);
+  backdrop-filter: blur(4px);
 
   display: flex;
   flex-direction: column;
