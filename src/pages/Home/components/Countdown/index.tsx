894d4bbf582819091703649c9579f8371e6976c4
Marc0sGabriel <mgabriel4747@gmail.com>
2023-09-28 19:22:56 -0300

    :sparkles: feat: add div to render properly

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -65,11 +65,19 @@ export function Countdown() {
 
   return (
     <CountDownContainer>
-      <span>{minutes[0]}</span>
-      <span>{minutes[1]}</span>
+      <div>
+        <span>{minutes[0]}</span>
+      </div>
+      <div>
+        <span>{minutes[1]}</span>
+      </div>
       <Separator>:</Separator>
-      <span>{seconds[0]}</span>
-      <span>{seconds[1]}</span>
+      <div>
+        <span>{seconds[0]}</span>
+      </div>
+      <div>
+        <span>{seconds[1]}</span>
+      </div>
     </CountDownContainer>
   );
 }
